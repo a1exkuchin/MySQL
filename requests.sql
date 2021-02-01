2aaf7545fec555e29be873e70049594adf5214f0
a1exkuchin <alexkuchin@list.ru>
2021-02-01 16:20:19 +0500

    initial commit

--- requests.sql
+++ requests.sql
@@ -26,3 +26,43 @@ GROUP BY
 ORDER BY
   department, soft;
 
+-- Представление выводящее количество купленного за все время ПО в таблицу `soft_itog` с колонками `soft_id`, `soft` и `itog`.
+
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `alex`@`%` 
+    SQL SECURITY DEFINER
+VIEW `soft_itog` AS
+    SELECT 
+        `license`.`soft_id` AS `soft_id`,
+        (SELECT 
+                `soft`.`name`
+            FROM
+                `soft`
+            WHERE
+                (`license`.`soft_id` = `soft`.`id`)) AS `soft`,
+        SUM(`license`.`number`) AS `itog`
+    FROM
+        `license`
+    GROUP BY `soft`
+
+-- Представление выводящее количество переданного подразделениям ПО за все время в таблицу `transfer_itog` с колонками `soft_id`, `soft` и `itog`.
+
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `alex`@`%` 
+    SQL SECURITY DEFINER
+VIEW `transfer_itog` AS
+    SELECT 
+        `transfer`.`license_soft_id` AS `soft_id`,
+        (SELECT 
+                `soft`.`name`
+            FROM
+                `soft`
+            WHERE
+                (`transfer`.`license_soft_id` = `soft`.`id`)) AS `soft`,
+        SUM(`transfer`.`number`) AS `itog`
+    FROM
+        `transfer`
+    GROUP BY `soft`
+
